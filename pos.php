6d98fea9a590cf9dc26b3d8f68ef52239e7fd542
llenny18 <ramosalleneid01@gmail.com>
2024-01-08 22:52:53 +0800

    pos start

--- pos.php
+++ pos.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<!-- Basic Page Info -->
 	<?php include("./includes.php"); ?>
-<meta charset="utf-8">
+	<meta charset="utf-8">
 	<title>DeskApp - Bootstrap Admin Dashboard HTML Template</title>
 
 	<!-- Site favicon -->
@@ -15,7 +16,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 	<!-- Google Font -->
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+		rel="stylesheet">
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="./vendors/styles/core.css">
 	<link rel="stylesheet" type="text/css" href="./vendors/styles/icon-font.min.css">
@@ -25,12 +27,13 @@
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
+		function gtag() { dataLayer.push(arguments); }
 		gtag('js', new Date());
 
 		gtag('config', 'UA-119386393-1');
 	</script>
 </head>
+
 <body>
 	<div class="pre-loader">
 		<div class="pre-loader-box">
@@ -44,7 +47,7 @@
 			</div>
 		</div>
 	</div>
-<?php include("./header.php"); ?>
+	<?php include("./header.php"); ?>
 
 	<div class="right-sidebar">
 		<div class="sidebar-title">
@@ -73,43 +76,57 @@
 				<h4 class="weight-600 font-18 pb-10">Menu Dropdown Icon</h4>
 				<div class="sidebar-radio-group pb-10 mb-10">
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebaricon-1" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-1" checked="">
+						<input type="radio" id="sidebaricon-1" name="menu-dropdown-icon" class="custom-control-input"
+							value="icon-style-1" checked="">
 						<label class="custom-control-label" for="sidebaricon-1"><i class="fa fa-angle-down"></i></label>
 					</div>
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebaricon-2" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-2">
+						<input type="radio" id="sidebaricon-2" name="menu-dropdown-icon" class="custom-control-input"
+							value="icon-style-2">
 						<label class="custom-control-label" for="sidebaricon-2"><i class="ion-plus-round"></i></label>
 					</div>
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebaricon-3" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-3">
-						<label class="custom-control-label" for="sidebaricon-3"><i class="fa fa-angle-double-right"></i></label>
+						<input type="radio" id="sidebaricon-3" name="menu-dropdown-icon" class="custom-control-input"
+							value="icon-style-3">
+						<label class="custom-control-label" for="sidebaricon-3"><i
+								class="fa fa-angle-double-right"></i></label>
 					</div>
 				</div>
 
 				<h4 class="weight-600 font-18 pb-10">Menu List Icon</h4>
 				<div class="sidebar-radio-group pb-30 mb-10">
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebariconlist-1" name="menu-list-icon" class="custom-control-input" value="icon-list-style-1" checked="">
-						<label class="custom-control-label" for="sidebariconlist-1"><i class="ion-minus-round"></i></label>
+						<input type="radio" id="sidebariconlist-1" name="menu-list-icon" class="custom-control-input"
+							value="icon-list-style-1" checked="">
+						<label class="custom-control-label" for="sidebariconlist-1"><i
+								class="ion-minus-round"></i></label>
 					</div>
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebariconlist-2" name="menu-list-icon" class="custom-control-input" value="icon-list-style-2">
-						<label class="custom-control-label" for="sidebariconlist-2"><i class="fa fa-circle-o" aria-hidden="true"></i></label>
+						<input type="radio" id="sidebariconlist-2" name="menu-list-icon" class="custom-control-input"
+							value="icon-list-style-2">
+						<label class="custom-control-label" for="sidebariconlist-2"><i class="fa fa-circle-o"
+								aria-hidden="true"></i></label>
 					</div>
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebariconlist-3" name="menu-list-icon" class="custom-control-input" value="icon-list-style-3">
+						<input type="radio" id="sidebariconlist-3" name="menu-list-icon" class="custom-control-input"
+							value="icon-list-style-3">
 						<label class="custom-control-label" for="sidebariconlist-3"><i class="dw dw-check"></i></label>
 					</div>
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebariconlist-4" name="menu-list-icon" class="custom-control-input" value="icon-list-style-4" checked="">
-						<label class="custom-control-label" for="sidebariconlist-4"><i class="icon-copy dw dw-next-2"></i></label>
+						<input type="radio" id="sidebariconlist-4" name="menu-list-icon" class="custom-control-input"
+							value="icon-list-style-4" checked="">
+						<label class="custom-control-label" for="sidebariconlist-4"><i
+								class="icon-copy dw dw-next-2"></i></label>
 					</div>
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebariconlist-5" name="menu-list-icon" class="custom-control-input" value="icon-list-style-5">
-						<label class="custom-control-label" for="sidebariconlist-5"><i class="dw dw-fast-forward-1"></i></label>
+						<input type="radio" id="sidebariconlist-5" name="menu-list-icon" class="custom-control-input"
+							value="icon-list-style-5">
+						<label class="custom-control-label" for="sidebariconlist-5"><i
+								class="dw dw-fast-forward-1"></i></label>
 					</div>
 					<div class="custom-control custom-radio custom-control-inline">
-						<input type="radio" id="sidebariconlist-6" name="menu-list-icon" class="custom-control-input" value="icon-list-style-6">
+						<input type="radio" id="sidebariconlist-6" name="menu-list-icon" class="custom-control-input"
+							value="icon-list-style-6">
 						<label class="custom-control-label" for="sidebariconlist-6"><i class="dw dw-next"></i></label>
 					</div>
 				</div>
@@ -120,7 +137,7 @@
 			</div>
 		</div>
 	</div>
-<?php include("./sidebar.php"); ?>
+	<?php include("./sidebar.php"); ?>
 	<div class="mobile-menu-overlay"></div>
 
 	<div class="main-container">
@@ -138,34 +155,64 @@
 									<li class="breadcrumb-item active" aria-current="page">Point Of Sale</li>
 								</ol>
 							</nav>
-							
+
 						</div>
 						<div class="col-md-6 col-sm-12 text-right">
-						<div class="dropdown">
-							<a class="btn btn-primary dropdown-toggle" href="#" role="button" data-toggle="dropdown">
-								View POS Cart
-							</a>
-						
+							<div class="dropdown">
+								<button class="btn btn-primary dropdown-toggle" onclick="showTable()" 
+									data-toggle="dropdown">
+									View POS Cart
+								</button>
+
+							</div>
 						</div>
 					</div>
-					</div>
-					
+
 				</div>
-				
+				<div id="tableContainer" style="display: none; margin-top: -100px;position: fixed; /* Stay in place */ z-index: 1;height: 500px; /* Full height */ overflow: auto; /* Enable scroll if needed */; width: 78%; background-color: white; border-radius: 20px; padding: 30px;">
+				<div class="table-responsive">
+						<table class="table table-striped" style="border-collapse: collapse; width: 100%; border: 2px solid lightgray; ">
+						<thead>
+							<tr>
+								<th scope="col">Product ID</th>
+								<th scope="col">Product Name</th>
+								<th scope="col">Quantity</th>
+								<th scope="col">Price</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th scope="row">54634612</th>
+								<td>Gatorade</td>
+								<td>4</td>
+								<td>45.67</td>
+							</tr>
+							
+						</tbody>
+					</table>
+				</div>
+				</div>
 				<div class="row clearfix">
 					<?php pos_items($conn); ?>
-					
-				</div>
 
+				</div>
+				
 
 			</div>
 			<?php include("./footer.php"); ?>
 		</div>
 	</div>
 	<!-- js -->
+	<script>
+		function showTable() {
+			var tableContainer = document.getElementById("tableContainer");
+			tableContainer.style.display = "block";
+		}
+	</script>
 	<script src="./vendors/scripts/core.js"></script>
 	<script src="./vendors/scripts/script.min.js"></script>
 	<script src="./vendors/scripts/process.js"></script>
 	<script src="./vendors/scripts/layout-settings.js"></script>
 </body>
+
 </html>
